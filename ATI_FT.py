4b24db8a84d97a209c481f14aff279ddb0ff305c
yifanzhu95 <yifanzhu95@gmail.com>
2025-04-11 17:40:13 -0400

    Added filtered version of the filter. Added testing code as well.

--- ATI_FT.py
+++ ATI_FT.py
@@ -4,6 +4,8 @@ import time, copy
 import pickle
 import threading
 from threading import Thread, Lock, RLock
+from scipy.signal import butter, filtfilt
+from collections import deque
 
 calmat = [[-0.009588917717, 0.21333759651, -0.211067259312, -7.340359210968, 0.099111407995, 7.244591712952],\
     [0.173573523760, 8.756484985352, -0.151811018586, -4.205108642578, -0.012327974662, -4.241057395935],\
@@ -61,6 +63,91 @@ class ATIDriver:
 
     def read(self):
         return copy.copy(self.reading.tolist())
+
+    def shutdown(self):
+        self._loopLock.acquire()
+        self.shut_down_flag = True
+        self._loopLock.release()
+
+
+
+# Apply the filter
+def butter_lowpass_filter(data, cutoff, fs, order):
+    b, a = butter_lowpass(cutoff, fs, order)
+    y = filtfilt(b, a, data)
+    return y
+
+
+class ATIDriverFilter:
+    def __init__(self, hz = 2000., cutoff = 100., history_length = 100) -> None:
+        self.hz = hz 
+        self.offset = np.zeros(6)
+        self.reading = np.zeros(6)
+        self._loopLock = RLock()
+
+        #Low-pass filter
+        nyq = 0.5 * hz  # Nyquist frequency
+        normal_cutoff = cutoff / nyq
+        self.b, self.a = butter(4, normal_cutoff, btype='low', analog=False)
+
+        #history
+        self.history = deque(maxlength=history_length)
+        self.history_length = history_length
+        #Daq board
+        self.task = nidaqmx.Task()
+        self.task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
+        self.task.ai_channels.add_ai_voltage_chan("Dev1/ai1")
+        self.task.ai_channels.add_ai_voltage_chan("Dev1/ai2")
+        self.task.ai_channels.add_ai_voltage_chan("Dev1/ai3")
+        self.task.ai_channels.add_ai_voltage_chan("Dev1/ai4")
+        self.task.ai_channels.add_ai_voltage_chan("Dev1/ai5")
+
+
+    def start(self):
+        controlThread = threading.Thread(target = self._loop)
+        self.shut_down_flag = False
+        controlThread.start()
+        self.fill_history()
+        print("ATIDriver:started")
+        return True
+
+    def fill_history(self):
+        while len(self.history) < self.history_length:
+            data = np.array(self.task.read())
+            for itemNum in range(len(data)):
+                data[itemNum] = data[itemNum] - self.offset[itemNum]
+            self.reading = np.transpose(np.matmul(calmat, np.transpose(data)))
+            self.history.append(self.reading)
+            time.sleep(1./self.hz)
+
+    def zero_sensor(self, N = 20):
+        test_total = self.task.read()
+        for testNum in range(N):
+            test = self.task.read()
+            for item in range(len(test_total)):
+                test_total[item] = test_total[item]+test[item]
+        zeros = [x/(N+1) for x in test_total]
+        self.offset = np.array(zeros)
+
+    def _loop(self):
+        """main control thread, synchronizing all components
+        in each loop,states are updated and new commands are issued
+        """
+        while not self.shut_down_flag:
+            self._loopLock.acquire()
+            data = np.array(self.task.read())
+            for itemNum in range(len(data)):
+                data[itemNum] = data[itemNum] - self.offset[itemNum]
+            self.reading = np.transpose(np.matmul(calmat, np.transpose(data)))
+            self.history.append(self.reading)
+            self._loopLock.release()
+            time.sleep(1./self.hz)
+        
+        print('ATIDriver: Exiting')
+
+    def read(self):
+        #return copy.copy(self.reading.tolist())
+        return filtfilt(self.b, self.a, np.array(self.history).T)[:,-1].tolist(), self.history[-1].tolist()
 
     def shutdown(self):
         self._loopLock.acquire()
--- FT_client.py
+++ FT_client.py
@@ -24,11 +24,41 @@ class FTClient:
         self.s.shutdown_ft_sensor()
 
 if __name__=="__main__":
-    ft_driver = FTClient('http://172.16.0.64:8080')
+    ## filtered version
+    hz = 2000
+    ft_driver = FTClient('http://172.16.0.1:8080')
     ft_driver.zero_ft_sensor()
     ft_driver.start_ft_sensor()
-    for i in range(100):
+    filtered_fts = []
+    raw_fts = []
+    for i in range(4000):
         start_time = time.time()
-        print(ft_driver.read_ft_sensor())
-        time.sleep(0.1)
+        filtered, raw = ft_driver.read_ft_sensor()
+        filtered_fts.append(filtered)
+        raw_fts.append(raw)
+        time.sleep(1/hz)
+
     ft_driver.shutdown_ft_sensor()
+
+    #visualize
+    t = np.linspace(0,2,4000)        
+    plt.figure(figsize=(10, 5))
+    plt.plot(t, np.array(raw_fts)[:,2], label='Noisy Signal Fz ')
+    plt.plot(t, np.array(filtered_fts)[:,2], label='Filtered Signal Fz', linewidth=2)
+    plt.title("Butterworth Low-Pass Filter")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Amplitude")
+    plt.legend()
+    plt.grid()
+    plt.tight_layout()
+    plt.show()
+
+    ##
+    # ft_driver = FTClient('http://172.16.0.1:8080')
+    # ft_driver.zero_ft_sensor()
+    # ft_driver.start_ft_sensor()
+    # for i in range(100):
+    #     start_time = time.time()
+    #     print(ft_driver.read_ft_sensor())
+    #     time.sleep(0.1)
+    # ft_driver.shutdown_ft_sensor()
